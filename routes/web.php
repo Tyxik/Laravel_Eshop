16b63ecebe9c9f952f3f4a9081d0c545a7f13837
Tyxik <tylmaxim@gmail.com>
2024-12-03 07:55:14 +0100

    navbar_contact

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\ContactController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
-
+Route::get('/contact', [ContactController::class, 'index'])->name('asi_contact.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
